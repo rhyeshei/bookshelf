f08fd78284db7ac3cbbad2206a3a59b4bf42b227
Ryusei takai <bbryusei7515@gmail.com>
2025-12-13 10:26:58 +0900

    first commit

---
+++ bookproject/book/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.index_view,name = 'index'),
+    path('book/',views.ListBookView.as_view(), name = 'list-book'), # 一覧画へのpath
+    path('book/<int:pk>/detail', views.DetailBookView.as_view(), name = 'detail-book'), #詳細画面へのpth
+    path('book/create/',views.CreateBookView.as_view(), name = 'create-book'), # 作成画面へのpath
+    path('book/<int:pk>/delete/', views.DeleteBookView.as_view(),name = 'delete-book'), #削除画面へのpath
+    path('book/<int:pk>/update/', views.UpdateBookView.as_view(), name ='update-book'), #削除画面へのpath
+    path('book/<int:book_id>/review/', views.CreateReviewView.as_view(), name = 'review')
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
